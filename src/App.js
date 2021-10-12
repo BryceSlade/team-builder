318596208354b7decf040417cb806309249123fc
Bryce Slade <bryceslade@gmail.com>
2021-10-12 12:10:14 -0600

    initial push

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <h2>Hello World</h2>
+    </div>
+  );
+}
+
+export default App;
+
+// hi
